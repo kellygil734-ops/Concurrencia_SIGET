79c0bd76bb60b5fb502d9ad3d2aa28beb5fa9148
kellygil734-ops <kelly.gil734@pascualbravo.edu.co>
2026-04-14 17:22:30 -0500

    Update main.cpp
    Removed the sensor and analysis module functions to simplify the code.

--- main.cpp
+++ main.cpp
@@ -12,45 +12,6 @@ using namespace std;
 const int CAPACIDAD_BUFFER = 5;
 queue<int> buffer_trafico;
 mutex mtx;
-condition_variable cv_producir, cv_consumir;
-
-// Función del Sensor (Productor)
-void sensor_trafico(int id) {
-    for (int i = 0; i < 5; ++i) { // 5 lecturas por sensor
-        unique_lock<mutex> lock(mtx);
-        
-        // Esperar si el búfer está lleno (Control de concurrencia)
-        cv_producir.wait(lock, [] { return buffer_trafico.size() < CAPACIDAD_BUFFER; });
-
-        int dato = rand() % 100; // Simulación de flujo vehicular
-        buffer_trafico.push(dato);
-        cout << "[SENSOR " << id << "] Detectado flujo: " << dato << " veh/min. Enviado al SIGET." << endl;
-
-        cv_consumir.notify_one(); // Notificar al módulo de análisis
-        lock.unlock();
-        
-        this_thread::sleep_for(chrono::milliseconds(800)); // Simular tiempo entre lecturas
-    }
-}
-
-// Función del Módulo de Análisis (Consumidor)
-void modulo_analisis() {
-    for (int i = 0; i < 10; ++i) { // Procesa el total de lecturas
-        unique_lock<mutex> lock(mtx);
-
-        // Esperar si no hay datos (Evitar lectura en vacío)
-        cv_consumir.wait(lock, [] { return !buffer_trafico.empty(); });
-
-        int dato = buffer_trafico.front();
-        buffer_trafico.pop();
-        cout << "   [ANALISIS] Procesando datos del SIGET... Flujo actual: " << dato << ". Ajustando semáforos." << endl;
-
-        cv_producir.notify_one(); // Notificar espacio disponible en búfer
-        lock.unlock();
-
-        this_thread::sleep_for(chrono::milliseconds(1200)); // Simular tiempo de procesamiento
-    }
-}
 
 int main() {
     cout << "--- INICIANDO SIMULACION DE CONCURRENCIA SIGET ---" << endl;
